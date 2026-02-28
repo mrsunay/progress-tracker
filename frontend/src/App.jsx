01ac7433f69f863be889ba00d20408eb2ff9b4e6
Sunny <mrsunay@gmail.com>
2026-02-27 21:01:04 -0600

    Implement POST function in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,9 @@ import axios from "axios"
 
 function App() {
   const [goals, setGoals] = useState([])
+  const [newTitle, setNewTitle] = useState("")
 
-  useEffect(() => {
-    const fetchGoals = async () => {
+      const fetchGoals = async () => {
       try {
         const response = await axios.get("http://127.0.0.1:8000/api/goals/")
         console.log("API response:", response.data)
@@ -15,16 +15,32 @@ function App() {
       }
     }
 
+
+  useEffect(() => {
     fetchGoals()
   }, [])
+  
+  const createGoal = async () => {
+    if (!newTitle.trim()) return
+
+    await axios.post("http://127.0.0.1:8000/api/goals/", {
+      title: newTitle
+    })
+    setNewTitle("")
+    fetchGoals()
+  }
+
 
   return (
-    <div>
+    <>
       <h1>Learning Goals</h1>
       {goals.map(goal => (
         <div key={goal.id}>{goal.title}</div>
       ))}
-    </div>
+      <input value={newTitle} onChange={(e)=> setNewTitle(e.target.value)} placeholder="Enter new goal"></input>
+    <button onClick={createGoal}>Create Goal</button>
+    </>
+    
   )
 }
 
