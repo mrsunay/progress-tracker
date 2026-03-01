738ed933a32b0a5d1fd9511bc324c8daacec086a
Sunny <mrsunay@gmail.com>
2026-03-01 16:17:59 -0600

    Add goal deletion with immediate state update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,12 +14,6 @@ function App() {
         console.error("Fetch error:", error)
       }
     }
-
-
-  useEffect(() => {
-    fetchGoals()
-  }, [])
-  
   const createGoal = async () => {
     if (!newTitle.trim()) return
   try {
@@ -32,11 +26,27 @@ function App() {
   console.error("Create error:", error)
 }
   }
+
+  const deleteGoal = async (id) => {
+    try {
+      await axios.delete(`http://127.0.0.1:8000/api/goals/${id}/`)
+      setGoals(prev => prev.filter(goal => goal.id !== id))
+    } catch (error) {
+      console.error("Delete error:", error)
+    }
+  }
+
+  useEffect(() => {
+    fetchGoals()
+  }, [])
+  
+
   return (
     <>
       <h1>Learning Goals</h1>
       {goals.map(goal => (
-        <div key={goal.id}>{goal.title}</div>
+        <div key={goal.id}>{goal.title}<button onClick={() => deleteGoal(goal.id)}>delete</button></div>
+        
       ))}
       <input value={newTitle} onChange={(e)=> setNewTitle(e.target.value)} placeholder="Enter new goal"></input>
     <button onClick={createGoal}>Create Goal</button>
